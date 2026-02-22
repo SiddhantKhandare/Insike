a83c009e5d779fabf3374f262e2b182cd2d269ea
SiddhantKhandare <sidraj.sk@gmail.com>
2026-02-22 23:40:38 +0530

    Bottom tabs updated

---
+++ src/screens/reels/ReelsScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const ReelsScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Reels Screen</Text>
+    </View>
+  );
+};
+
+export default ReelsScreen;
