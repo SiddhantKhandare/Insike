38320ca99c921d4a0991a2ea51673cbd9cbf2595
SiddhantKhandare <sidraj.sk@gmail.com>
2026-02-24 23:48:39 +0530

    Post and Stories data updated

---
+++ src/data/dummyPosts.js
@@ -0,0 +1,18 @@
+export const dummyPosts = [
+  {
+    id: "1",
+    username: "rohit",
+    profilePic: "https://i.pravatar.cc/150?img=6",
+    postImage: "https://picsum.photos/600/600?random=1",
+    caption: "Enjoying the view 🌄",
+    likes: 128,
+  },
+  {
+    id: "2",
+    username: "neha",
+    profilePic: "https://i.pravatar.cc/150?img=7",
+    postImage: "https://picsum.photos/600/600?random=2",
+    caption: "Beautiful day ❤️",
+    likes: 342,
+  },
+];
---
+++ src/data/dummyStories.js
@@ -0,0 +1,7 @@
+export const dummyStories = [
+  { id: "1", username: "your_story", image: "https://i.pravatar.cc/150?img=1" },
+  { id: "2", username: "rohit", image: "https://i.pravatar.cc/150?img=2" },
+  { id: "3", username: "neha", image: "https://i.pravatar.cc/150?img=3" },
+  { id: "4", username: "amit", image: "https://i.pravatar.cc/150?img=4" },
+  { id: "5", username: "sneha", image: "https://i.pravatar.cc/150?img=5" },
+];
