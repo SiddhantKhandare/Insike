51719021828d9bf42ad6b52204910ef1d6d67711
SiddhantKhandare <sidraj.sk@gmail.com>
2026-03-06 23:58:06 +0530

    StoryItem code updated

--- src/components/story/StoryItem.js
+++ src/components/story/StoryItem.js
@@ -1,10 +1,20 @@
 import React from "react";
 import { View, Text, Image, StyleSheet } from "react-native";
+import LinearGradient from "react-native-linear-gradient";
 
 const StoryItem = ({ item }) => {
   return (
     <View style={styles.container}>
-      <Image source={{ uri: item.image }} style={styles.image} />
+      
+      <LinearGradient
+        colors={["#feda75", "#fa7e1e", "#d62976", "#962fbf", "#4f5bd5"]}
+        style={styles.gradientBorder}
+      >
+        <View style={styles.imageContainer}>
+          <Image source={{ uri: item.image }} style={styles.image} />
+        </View>
+      </LinearGradient>
+
       <Text style={styles.username} numberOfLines={1}>
         {item.username}
       </Text>
@@ -19,15 +29,34 @@ const styles = StyleSheet.create({
     alignItems: "center",
     marginHorizontal: 8,
   },
-  image: {
-    width: 70,
-    height: 70,
-    borderRadius: 35,
-    borderWidth: 2,
-    borderColor: "#ff8501",
+
+  gradientBorder: {
+    width: 74,
+    height: 74,
+    borderRadius: 37,
+    justifyContent: "center",
+    alignItems: "center",
   },
+
+  imageContainer: {
+    width: 68,
+    height: 68,
+    borderRadius: 34,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  image: {
+    width: 62,
+    height: 62,
+    borderRadius: 31,
+  },
+
   username: {
     fontSize: 12,
     marginTop: 4,
+    maxWidth: 70,
+    textAlign: "center",
   },
 });
