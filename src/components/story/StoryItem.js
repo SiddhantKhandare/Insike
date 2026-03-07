6c8b42cdc18688c38b88ab4e0bfe445b927c0d22
SiddhantKhandare <sidraj.sk@gmail.com>
2026-03-07 23:43:32 +0530

    Added dummy data of Story

--- src/components/story/StoryItem.js
+++ src/components/story/StoryItem.js
@@ -1,19 +1,31 @@
 import React from "react";
 import { View, Text, Image, StyleSheet } from "react-native";
 import LinearGradient from "react-native-linear-gradient";
+import Icon from "react-native-vector-icons/Ionicons";
 
 const StoryItem = ({ item }) => {
+  const isYourStory = item.isYourStory;
+
   return (
     <View style={styles.container}>
-      
-      <LinearGradient
-        colors={["#feda75", "#fa7e1e", "#d62976", "#962fbf", "#4f5bd5"]}
-        style={styles.gradientBorder}
-      >
-        <View style={styles.imageContainer}>
+      {isYourStory ? (
+        <View style={styles.yourStoryContainer}>
           <Image source={{ uri: item.image }} style={styles.image} />
+
+          <View style={styles.plusIcon}>
+            <Icon name="add" size={14} color="#fff" />
+          </View>
         </View>
-      </LinearGradient>
+      ) : (
+        <LinearGradient
+          colors={["#feda75", "#fa7e1e", "#d62976", "#962fbf", "#4f5bd5"]}
+          style={styles.gradientBorder}
+        >
+          <View style={styles.imageContainer}>
+            <Image source={{ uri: item.image }} style={styles.image} />
+          </View>
+        </LinearGradient>
+      )}
 
       <Text style={styles.username} numberOfLines={1}>
         {item.username}
@@ -51,6 +63,25 @@ const styles = StyleSheet.create({
     width: 62,
     height: 62,
     borderRadius: 31,
+  },
+
+  yourStoryContainer: {
+    width: 70,
+    height: 70,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  plusIcon: {
+    position: "absolute",
+    bottom: 0,
+    right: 0,
+    backgroundColor: "#0095f6",
+    width: 20,
+    height: 20,
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
   },
 
   username: {
--- src/data/dummyStories.js
+++ src/data/dummyStories.js
@@ -1,7 +1,28 @@
 export const dummyStories = [
-  { id: "1", username: "your_story", image: "https://i.pravatar.cc/150?img=1" },
-  { id: "2", username: "rohit", image: "https://i.pravatar.cc/150?img=2" },
-  { id: "3", username: "neha", image: "https://i.pravatar.cc/150?img=3" },
-  { id: "4", username: "amit", image: "https://i.pravatar.cc/150?img=4" },
-  { id: "5", username: "sneha", image: "https://i.pravatar.cc/150?img=5" },
+  {
+    id: "0",
+    username: "Your Story",
+    image: "https://i.pravatar.cc/150?img=10",
+    isYourStory: true,
+  },
+  {
+    id: "1",
+    username: "rohit",
+    image: "https://i.pravatar.cc/150?img=2",
+  },
+  {
+    id: "2",
+    username: "neha",
+    image: "https://i.pravatar.cc/150?img=3",
+  },
+  {
+    id: "3",
+    username: "amit",
+    image: "https://i.pravatar.cc/150?img=4",
+  },
+  {
+    id: "4",
+    username: "sneha",
+    image: "https://i.pravatar.cc/150?img=5",
+  },
 ];
