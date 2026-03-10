c463d94b32433ed05651fa70506558e338a4da2c
SiddhantKhandare <sidraj.sk@gmail.com>
2026-03-10 23:59:48 +0530

    reels page updated

---
+++ src/data/dummyReels.js
@@ -0,0 +1,16 @@
+export const dummyReels = [
+  {
+    id: "1",
+    video:
+      "https://www.w3schools.com/html/mov_bbb.mp4",
+    username: "rohit",
+    caption: "Beautiful nature 🌿",
+  },
+  {
+    id: "2",
+    video:
+      "https://www.w3schools.com/html/movie.mp4",
+    username: "neha",
+    caption: "Travel vibes ✈️",
+  },
+];
