a60a38b35fd20e5dad6f259aadebac0e1bfb24e2
SiddhantKhandare <sidraj.sk@gmail.com>
2026-03-12 21:35:17 +0530

    ReelsItem page code updated

--- src/components/reels/ReelItem.js
+++ src/components/reels/ReelItem.js
@@ -1,11 +1,13 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, Image } from "react-native";
 import Video from "react-native-video";
+import Icon from "react-native-vector-icons/Ionicons";
 
 const ReelItem = ({ item }) => {
   return (
     <View style={styles.container}>
       
+      {/* Video */}
       <Video
         source={{ uri: item.video }}
         style={styles.video}
@@ -13,10 +15,40 @@ const ReelItem = ({ item }) => {
         repeat
       />
 
-      {/* Overlay Text */}
-      <View style={styles.overlay}>
-        <Text style={styles.username}>{item.username}</Text>
+      {/* Bottom Left Content */}
+      <View style={styles.bottomLeft}>
+        <View style={styles.userRow}>
+          <Image
+            source={{ uri: "https://i.pravatar.cc/150?img=12" }}
+            style={styles.profile}
+          />
+          <Text style={styles.username}>{item.username}</Text>
+        </View>
+
         <Text style={styles.caption}>{item.caption}</Text>
+      </View>
+
+      {/* Right Side Actions */}
+      <View style={styles.rightActions}>
+        
+        <View style={styles.actionItem}>
+          <Icon name="heart-outline" size={28} color="#fff" />
+          <Text style={styles.actionText}>245K</Text>
+        </View>
+
+        <View style={styles.actionItem}>
+          <Icon name="chatbubble-outline" size={26} color="#fff" />
+          <Text style={styles.actionText}>1.2K</Text>
+        </View>
+
+        <View style={styles.actionItem}>
+          <Icon name="paper-plane-outline" size={26} color="#fff" />
+        </View>
+
+        <View style={styles.actionItem}>
+          <Icon name="bookmark-outline" size={26} color="#fff" />
+        </View>
+
       </View>
 
     </View>
@@ -37,20 +69,51 @@ const styles = StyleSheet.create({
     position: "absolute",
   },
 
-  overlay: {
+  bottomLeft: {
     position: "absolute",
     bottom: 40,
     left: 15,
   },
 
+  userRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 6,
+  },
+
+  profile: {
+    width: 35,
+    height: 35,
+    borderRadius: 20,
+    marginRight: 10,
+  },
+
   username: {
     color: "#fff",
     fontWeight: "bold",
-    fontSize: 16,
+    fontSize: 15,
   },
 
   caption: {
     color: "#fff",
+    maxWidth: 250,
+  },
+
+  rightActions: {
+    position: "absolute",
+    right: 15,
+    bottom: 100,
+    alignItems: "center",
+  },
+
+  actionItem: {
+    alignItems: "center",
+    marginBottom: 18,
+  },
+
+  actionText: {
+    color: "#fff",
+    fontSize: 12,
     marginTop: 4,
   },
 });
